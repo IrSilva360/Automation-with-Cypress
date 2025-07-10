e8608214994a31e42b8b8195335517ccaf08d959
Iranildo Silva <iran.infor.360@gmail.com>
2025-07-10 17:53:21 -0300

    Added Selector list

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,13 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']",
+  }
+
   it('Login - Success', () => { // ao usar ".skip" o comando será ignorado, serve para rodar o teste de forma parcial
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')// no comando ".visit()" informamos o endereço da pagina a ser testada
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') // o comando ".get()" siginifica procure, já o comando ".type()" é para inserir um texto, seja ele logim, senha ou qualquer que seja a informaçao que o formulario da pagina solicite
@@ -16,4 +25,24 @@ describe('Orange HRM Tests', () => {
     cy.get('.oxd-button').click()
     cy.get("[role='alert']")
   })
-})  
+  
+  // esta versão estamos ussando a lista de seletores, para se houver alguma alteração não precissar alterar todo o codigo. facilita pois só alteramos na lista.
+  it('Login - Success', () => { 
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type('Admin') // item da linha 4
+    cy.get(selectorsList.passwordField).type('admin123') // item da linha 5
+    cy.get(selectorsList.loginButton).click() // item da linha 6
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index') 
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard') // item da linha 7
+    
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type('Test')  
+    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert) // item da linha 8
+
+})
+
+})
